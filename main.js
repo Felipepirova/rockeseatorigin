82a840d13b4059b8f1264b6aff8e3aaf8f8a9bcd
Felipe Pirovano <felipepirovano95@gmail.com>
2021-10-07 19:21:53 -0300

    Final aula 5 - Origin

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 const links = document.querySelectorAll('nav ul li a')
+const sections = document.querySelectorAll('main section[id]')
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
@@ -33,13 +34,42 @@ function changeHeaderWhenScroll() {
   }
 }
 
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
 const swiper = new Swiper('.swiper-container', {
   sliderPerView: 1,
   pagination: {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      sliderPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 const scrollReveal = ScrollReveal({
@@ -63,4 +93,5 @@ scrollReveal.reveal(
 window.addEventListener('scroll', function () {
   backToTop()
   changeHeaderWhenScroll()
+  activateMenuAtCurrentSection()
 })
