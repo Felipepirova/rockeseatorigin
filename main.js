5c6cd07dbbadef37399777eacca7b0409f1ff397
Felipe Pirovano <felipepirovano95@gmail.com>
2021-10-07 16:59:21 -0300

    Final Aula 4 - Origin

--- main.js
+++ main.js
@@ -16,13 +16,22 @@ for (const link of links) {
   })
 }
 
-window.addEventListener('scroll', function () {
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
-})
+}
 
 const swiper = new Swiper('.swiper-container', {
   sliderPerView: 1,
@@ -45,7 +54,13 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
+  footer .brand, footer .social
 `,
   { interval: 100 }
 )
+
+window.addEventListener('scroll', function () {
+  backToTop()
+  changeHeaderWhenScroll()
+})
